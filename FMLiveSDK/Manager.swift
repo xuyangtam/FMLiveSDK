84efcdf0a4e4aeb10b59cfcb4331087e02623cd8
xuyangtam <115985057+xuyangtam@users.noreply.github.com>
2023-12-24 16:57:04 +0800

    first

---
+++ FMLiveSDK/Manager.swift
@@ -0,0 +1,15 @@
+//
+//  Manager.swift
+//  FMLiveSDK
+//
+//  Created by xuyang Tan on 2023/12/23.
+//
+
+import Foundation
+
+public class Manager: NSObject {
+    
+    public func sayHello() {
+        print("say hello")
+    }
+}
---
+++ FMLiveSDK_Demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FMLiveSDK_Demo
+//
+//  Created by xuyang Tan on 2023/12/23.
+//
+
+import UIKit
+import FMLiveSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Manager().sayHello()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
